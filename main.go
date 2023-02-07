80a4f4e176ecaba6c0277325d591c1bd0dc3fd07
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-07 20:49:21 +0600

    checking how function works

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Printf("Hello World");
+
+	// var name string
+	// name = "Istiyak Amin"
+	// fmt.Println("Hello", name);
+
+	var a int = 10;
+	var b int = 20;
+
+	sum_result := sum(a, b)
+	fmt.Println(sum_result);
+
+}
+
+func sum(a int, b int) int {
+	return a+b;
 }
