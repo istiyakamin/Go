dd4f1049c7d329a7542ad2e928d12f277272507e
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-09 23:27:22 +0600

    Create a program that shows your address

---
+++ Code/Create a program that shows your address/main.go
@@ -0,0 +1,12 @@
+// Create a program that shows your name
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("House: L-41/D")
+	fmt.Println("Road: 03")
+	fmt.Println("Ward: 13")
+	fmt.Println("Police Station: Khulshi")
+	fmt.Println("City: Chattogram")
+}
