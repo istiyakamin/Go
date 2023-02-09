ede170126453ac7c74a64e617d541f933737650a
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-09 23:24:04 +0600

    Create a program that shows your name

---
+++ Code/Create a program that shows your name/main.go
@@ -0,0 +1,8 @@
+// Create a program that shows your name
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello Istiyak Amin")
+}
--- Code/print_ascii_code.go
+++ Code/print_ascii_code.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main()  {
+
+	// var name string
+	// name = "Istiyak Amin"
+	// fmt.Println("Hello", name);
+
+	// var a int = 10;
+	// var b int = 20;
+
+
+}
