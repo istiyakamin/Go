67287c4412dd6b34ecc78827edb437d3339041e0
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-09 23:32:27 +0600

    Create a program with multiple string variables

---
+++ Code/Create a program with multiple string variables/main.go
@@ -0,0 +1,11 @@
+// Create a program with multiple string variables
+package main
+
+import "fmt"
+
+func main(){
+	var first_name string = "Istiyak"
+	var last_name string = "Amin"
+
+	fmt.Println(first_name + " " + last_name)
+}
