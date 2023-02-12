42821f2ec99c6bc9e8dd8b07319fecb31cea0409
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-12 22:45:50 +0600

    dynamic array with trimmed

--- Code/Slices/main.go
+++ Code/Slices/main.go
@@ -35,13 +35,13 @@ func main() {
     t := []string{"g", "h", "i"}
     fmt.Println("dcl:", t)
 
-    // twoD := make([][]int, 3)
-    // for i := 0; i < 3; i++ {
-    //     innerLen := i + 1
-    //     twoD[i] = make([]int, innerLen)
-    //     for j := 0; j < innerLen; j++ {
-    //         twoD[i][j] = i + j
-    //     }
-    // }
-    // fmt.Println("2d: ", twoD)
+    twoD := make([][]int, 3)
+    for i := 0; i < 3; i++ {
+        innerLen := i + 1
+        twoD[i] = make([]int, innerLen)
+        for j := 0; j < innerLen; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
 }
