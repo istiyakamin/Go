46241163c76fb1efab4eeb40dc0a66fdebfdde81
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-12 21:40:26 +0600

    for loop

---
+++ Code/For/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	for {
+        fmt.Println("Ever Number:")
+		break
+    }
+
+	for k := 0; k < 10; k++ {
+		if(k%2 == 0){
+			fmt.Println(k)
+		}
+	}
+}
+
