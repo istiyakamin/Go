0ff95607ca3232e9d55787f19b857c473ce12c64
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-12 22:37:36 +0600

    basic slices

--- Code/Slices/main.go
+++ Code/Slices/main.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 
-    // s := make([]string, 3)
-    // fmt.Println("emp:", s)
+    s := make([]string, 3)
+    fmt.Println("emp:", s)
 
-    // s[0] = "a"
-    // s[1] = "b"
-    // s[2] = "c"
-    // fmt.Println("set:", s)
-    // fmt.Println("get:", s[2])
+    s[0] = "a"
+    s[1] = "b"
+    s[2] = "c"
+    fmt.Println("set:", s)
+    fmt.Println("get:", s[2])
 
     // fmt.Println("len:", len(s))
 
