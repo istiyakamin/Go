f5438498682c7e15784863e05f0a4f3384e3b377
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-12 21:47:22 +0600

    switch statement

---
+++ Switch/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i:=1; i<8; i++ {
+
+		switch i {
+		case 1:
+			fmt.Println("One")
+		case 2:
+			fmt.Println("Two")
+		case 3:
+			fmt.Println("Three")
+		case 4:
+			fmt.Println("Four")
+		
+		
+		}
+
+	}
+}
