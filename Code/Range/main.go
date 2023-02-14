fbc16cfedb0629c1979fba3a2a132fa28ca59948
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-14 21:55:03 +0600

    for key vaue range

--- Code/Range/main.go
+++ Code/Range/main.go
@@ -17,10 +17,10 @@ func main() {
         }
     }
 
-    // kvs := map[string]string{"a": "apple", "b": "banana"}
-    // for k, v := range kvs {
-    //     fmt.Printf("%s -> %s\n", k, v)
-    // }
+    kvs := map[string]string{"a": "apple", "b": "banana"}
+    for k, v := range kvs {
+        fmt.Printf("%s -> %s\n", k, v)
+    }
 
     // for k := range kvs {
     //     fmt.Println("key:", k)
