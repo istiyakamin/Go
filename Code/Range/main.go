6d9f4d3f7f8078e64a600ac797b83f093393def7
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-14 21:44:00 +0600

    for range

--- Code/Range/main.go
+++ Code/Range/main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 
     nums := []int{2, 3, 4}
-    // sum := 0
-    // for _, num := range nums {
-    //     sum += num
-    // }
-    // fmt.Println("sum:", sum)
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
 
     // for i, num := range nums {
     //     if num == 3 {
