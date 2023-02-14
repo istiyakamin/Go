fe9f027acd26e7767bdf8ddbb641e953e2a945a1
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-14 22:01:03 +0600

    go function plus

--- Code/Function/main.go
+++ Code/Function/main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-// func plus(a int, b int) int {
+func plus(a int, b int) int {
 
-//     return a + b
-// }
+    return a + b
+}
 
 // func plusPlus(a, b, c int) int {
 //     return a + b + c
@@ -13,8 +13,8 @@ import "fmt"
 
 func main() {
 
-    // res := plus(1, 2)
-    // fmt.Println("1+2 =", res)
+    res := plus(1, 2)
+    fmt.Println("1+2 =", res)
 
     // res = plusPlus(1, 2, 3)
     // fmt.Println("1+2+3 =", res)
