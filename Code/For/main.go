a668a2b81306b391ff4a4f991ad670ee09672154
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-19 22:52:22 +0600

    go1

--- Code/For/main.go
+++ Code/For/main.go
@@ -1,21 +1,21 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	for i := 1; i < 10; i++ {
-		fmt.Println(i)
-	}
+// func main() {
+// 	for i := 1; i < 10; i++ {
+// 		fmt.Println(i)
+// 	}
 
-	for {
-        fmt.Println("Ever Number:")
-		break
-    }
+// 	for {
+//         fmt.Println("Ever Number:")
+// 		break
+//     }
 
-	for k := 0; k < 10; k++ {
-		if(k%2 == 0){
-			fmt.Println(k)
-		}
-	}
-}
+// 	for k := 0; k < 10; k++ {
+// 		if(k%2 == 0){
+// 			fmt.Println(k)
+// 		}
+// 	}
+// }
 
