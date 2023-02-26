bdf2d1326a094ee73223d65c1e65d52ac0184299
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-26 13:52:22 +0600

    request endpoint done

--- Code/net/main.go
+++ Code/net/main.go
@@ -7,10 +7,27 @@ import (
 
 func main(){
 
+
 	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/service", service)
+	http.HandleFunc("/contact", contact)
+
 	http.ListenAndServe(":8888", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, `Welcome to homepage`);
+}
+
+func about(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, `Welcome to about`);
+}
+
+func service(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, `Welcome to service`);
+}
+
+func contact(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, `Welcome to contact`);
 }
