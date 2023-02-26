176c583c2bd5d83dbd55216a6c40a57c3e3eaba3
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-02-26 13:50:08 +0600

    run http server

---
+++ Code/net/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":8888", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, `Welcome to homepage`);
+}
